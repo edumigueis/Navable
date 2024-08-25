64f7a7b76a15ddb776ee26680a369f0e7a9ff225
eduardo <edmigueis@hotmail.com>
2024-08-24 23:51:57 -0300

    API added

---
+++ navable-api/src/main/java/com/unicamp/navable_api/NavableApiApplication.java
@@ -0,0 +1,13 @@
+package com.unicamp.navable_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavableApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NavableApiApplication.class, args);
+	}
+
+}
---
+++ navable-api/src/test/java/com/unicamp/navable_api/NavableApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicamp.navable_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavableApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
