670e0cfaa2557bcb123e578136b05f4013b5766a
hugo-mateus <hugocardosoveloso@gmail.com>
2025-05-20 22:24:46 -0300

    Fixes code smell 2- duplication in EstabelecimentoRepository and OcorrenciaRepository

--- navable-api/src/main/java/com/unicamp/navable_api/persistance/repositories/EstabelecimentoRepository.java
+++ navable-api/src/main/java/com/unicamp/navable_api/persistance/repositories/EstabelecimentoRepository.java
@@ -1,51 +1,52 @@
 package com.unicamp.navable_api.persistance.repositories;
 
 import com.unicamp.navable_api.persistance.entities.Estabelecimento;
-import org.springframework.data.jpa.repository.*;
+import com.unicamp.navable_api.persistance.support.GeoLocationSupport;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface EstabelecimentoRepository extends JpaRepository<Estabelecimento, Integer> {
+    // Revised to use constant
+    @Query(value = "SELECT e.id_estabelecimento, " +
+           "e.id_tipo_estabeleci, " +
+           "e.nome, " +
+           "e.latitude, " +
+           "e.longitude, " +
+           "e.imagem, " +
+           "e.endereco, " +
+           "AVG(a.nota) AS nota " +
+           "FROM estabelecimento e " +
+           "LEFT JOIN avaliacao a ON e.id_estabelecimento = a.id_estabelecimento " +
+           "WHERE " + GeoLocationSupport.ESTABELECIMENTO_HAVERSINE_DISTANCE + " " +
+           "GROUP BY e.id_estabelecimento", 
+           nativeQuery = true)
+    List<Object[]> findNearbyWithRatings(
+        @Param("latitude") double latitude,
+        @Param("longitude") double longitude,
+        @Param("distance") double distance
+    );
+    
+    // Providing a convenience method with default distance
+    default List<Object[]> findNearbyWithRatings(double latitude, double longitude) {
+        return findNearbyWithRatings(latitude, longitude, GeoLocationSupport.DEFAULT_SEARCH_RADIUS_KM);
+    }
 
-    // Query to find establishments within 1km of a given latitude and longitude
-    @Query(value = """
-            SELECT e.id_estabelecimento,
-                   e.id_tipo_estabeleci,
-                   e.nome,
-                   e.latitude,
-                   e.longitude,
-                   e.imagem,
-                   e.endereco,
-                   AVG(a.nota) AS nota
-            FROM estabelecimento e
-            LEFT JOIN avaliacao a ON e.id_estabelecimento = a.id_estabelecimento
-            WHERE (
-                6371 * acos(
-                    cos(radians(:latitude)) * cos(radians(e.latitude)) *
-                    cos(radians(e.longitude) - radians(:longitude)) +
-                    sin(radians(:latitude)) * sin(radians(e.latitude))
-                )
-            ) <= 1
-            GROUP BY e.id_estabelecimento
-            """, nativeQuery = true)
-    List<Estabelecimento> findNearby(@Param("latitude") double latitude, @Param("longitude") double longitude);
-
-    // Query to find establishments by rating, category, and type
     @Query(value = """
             SELECT e.*
             FROM Estabelecimento e
             JOIN Avaliacao a ON e.id_estabelecimento = a.id_estabelecimento
             JOIN CategoriaAceTipoEstab cat ON e.id_tipo_estabeleci = cat.id_tipo_estabeleci
             WHERE (a.nota = :nota OR :nota IS NULL)
-            AND (cat.categoria_ac_id IN (:lista_categoria) OR :lista_categoria IS NULL)
-            AND (e.id_tipo_estabeleci = :id_tipo_estabeleci OR :id_tipo_estabeleci IS NULL)
+            AND (:lista_categoria IS NULL OR cat.categoria_ac_id IN (:lista_categoria))
+            AND (:id_tipo_estabeleci IS NULL OR e.id_tipo_estabeleci = :id_tipo_estabeleci)
             """, nativeQuery = true)
-    List<Estabelecimento> findByNotaAndCategoriaAndTipo(@Param("nota") Float nota, @Param("lista_categoria") List<Integer> listaCategoria, @Param("id_tipo_estabeleci") Integer idTipoEstabeleci);
+    List<Estabelecimento> findByNotaAndCategoriaAndTipo(@Param("nota") Float nota,
+                                                         @Param("lista_categoria") List<Integer> listaCategoria,
+                                                         @Param("id_tipo_estabeleci") Integer idTipoEstabeleci);
 
-    // Query to search establishments by name
     @Query(value = """
             SELECT e.*
             FROM Estabelecimento e
