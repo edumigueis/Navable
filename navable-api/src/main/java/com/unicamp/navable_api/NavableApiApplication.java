ea925c17f1cd86dfc1b02ea7e64c96df9e8ec3c4
Eduardo Migueis <edmigueis@hotmail.com>
2025-06-15 23:00:42 -0300

    fix: auth

--- navable-api/src/main/java/com/unicamp/navable_api/NavableApiApplication.java
+++ navable-api/src/main/java/com/unicamp/navable_api/NavableApiApplication.java
@@ -1,12 +1,12 @@
 package com.unicamp.navable_api;
 
-import com.unicamp.navable_api.utils.CorsConfiguration;
+import com.unicamp.navable_api.utils.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(CorsConfiguration.class)
+@Import({CorsConfiguration.class, SecurityConfig.class})
 public class NavableApiApplication {
 
     public static void main(String[] args) {
